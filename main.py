2939dce98091a046dfde692b3a40438d524aeb93
bellfish <Samuelhorobin461@gmail.com>
2023-03-24 01:49:35 +0000

    Ability to retrieve and copy jar data

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import os
 import shutil
+import pathlib
 
 class MissingFileError(Exception):
     def __init__(self, directory):
-        self.message = f"ERROR :: {directory} does not exist"
+        self.message = f"{directory} does not exist"
         super().__init__(self.message)
 
+class ExistingFileError(Exception):
+    def __init__(self, directory):
+        self.message = f"{directory} already exists"
+        super().__init__(self.message)
 
 def getJarPath(version: str) -> str: 
     directory = os.path.join(os.getenv('APPDATA'), ".minecraft")
@@ -19,12 +24,18 @@ def getJarPath(version: str) -> str:
     jarPath = os.path.join(versionPath, f"{version}.jar")
     if not os.path.exists(jarPath):
         raise MissingFileError(jarPath)
+    
+    return jarPath
 
+def copyJarData(src, version):
+    dest = os.path.join(pathlib.Path(__file__).parent, "resource packs", f"{version}.jar")
+    print(src, dest)
+    jarCopy = shutil.copy(src, dest)
+    
 def extractJarData(path):
     pass
 
-
-getJarPath("1.19.2")
+copyJarData(getJarPath("1.19.2"), "1.19.2")
 
 
 
