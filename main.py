5f42f396e4f02cabfd2995685a1d8568bf5d9e9f
bellfish <Samuelhorobin461@gmail.com>
2023-03-26 23:37:07 +0100

    Scrapped main idea, added code

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import shutil
 import pathlib
+import zipfile
+import filecmp
+from PIL import Image
 
 class MissingFileError(Exception):
     def __init__(self, directory):
@@ -12,12 +15,12 @@ class ExistingFileError(Exception):
         self.message = f"{directory} already exists"
         super().__init__(self.message)
 
-def getJarPath(version: str) -> str: 
+def get_jar_path(version: str) -> str: 
     directory = os.path.join(os.getenv('APPDATA'), ".minecraft")
     if not os.path.exists(directory):
         raise MissingFileError(directory)
     
-    versionPath = os.path.join(directory, "versions", version)
+    versionPath = os.path.join(directory, "versions", str(version))
     if not os.path.exists(versionPath):
         raise MissingFileError(versionPath)
     
@@ -27,14 +30,48 @@ def getJarPath(version: str) -> str:
     
     return jarPath
 
-def copyJarData(src: str, version: str):
-    dest = os.path.join(pathlib.Path(__file__).parent, "resource packs", f"{version}.jar")
-    print(src, dest)
-    jarCopy = shutil.copy(src, dest)
-    
-def extractJarData(path: str, version: str):
-    shutil.make_archive(version, 'zip', path)
+def copy_jar_data(src: str, dest: str): shutil.copy(src, dest)
 
-copyJarData(getJarPath("1.19.2"), "1.19.2")
+def extract_textures(srcJar, folderPath, outputDir):
+    try:
+        with zipfile.ZipFile(srcJar) as jarFile:
+            for filePath in jarFile.namelist():
+                if filePath.startswith(folderPath):
+                    subPath = os.path.relpath(filePath, folderPath)
+                    subPath = subPath.replace('/', os.path.sep)  # replace forward slashes with backslashes
+                    output_path = os.path.join(outputDir, subPath)
+                    if os.path.isdir(filePath):
+                        os.makedirs(output_path, exist_ok=True)
+                    else:
+                        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+                        with open(output_path, 'wb') as outputFile:
+                            outputFile.write(jarFile.read(filePath))
+    except zipfile.BadZipFile as error:
+        print(error)
+
+def get_default_textures(version):
+    jarDest = os.path.join(pathlib.Path(__file__).parent, "resource packs", f"{version}.jar")
+    textureDest = os.path.join(pathlib.Path(__file__).parent, "resource packs", f"{version}")
+
+    if not os.path.exists(textureDest):
+        jarSrc = get_jar_path(version)
+        print(f"Copying jar data from {jarSrc}...")
+        copy_jar_data(jarSrc, jarDest)
+        print(f"Jar data copied to {jarDest}")
+
+        print(f"Extracting texure folder from {jarDest}...")
+        extract_textures(jarDest, "assets", textureDest)
+        os.remove(jarDest)
+        print(f"Extracted texture to {textureDest}")
+
+        return textureDest
+    return textureDest
+
+if __name__ == "__main__":
+    version = input("Enter texture pack version\n")
+    defaultTexturePath = get_default_textures(version)
+
+    
+
 
 
